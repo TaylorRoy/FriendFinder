b007278e783ce95713f15fe205f9f3cfc1d21195
TaylorRoy <tbroy@hotmail.com>
2018-10-26 22:29:08 -0600

    match almost done

--- server.js
+++ server.js
@@ -15,54 +15,6 @@ var PORT = 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// friend finder (DATA)
-// =============================================================
-// var users = [
-//     {
-//         routeName: "david",
-//         name: "David",
-//         bed: 3,
-//         car: 1,
-//         beverage: 2,
-//         sunday: 3,
-//         sports: 3,
-//         politics: 5,
-//         books: 2,
-//         pizza: 4,
-//         dayoff: 5,
-//         color: 1
-//     },
-//     {
-//         routeName: "liz",
-//         name: "Liz",
-//         bed: 5,
-//         car: 5,
-//         beverage: 1,
-//         sunday: 4,
-//         sports: 1,
-//         politics: 1,
-//         books: 5,
-//         pizza: 2,
-//         dayoff: 1,
-//         color: 5
-//     },
-//     {
-//         routeName: "forest",
-//         name: "Forest",
-//         bed: 2,
-//         car: 4,
-//         beverage: 2,
-//         sunday: 4,
-//         sports: 4,
-//         politics: 2,
-//         books: 1,
-//         pizza: 2,
-//         dayoff: 3,
-//         color: 3 
-//     }
-// ];
-
-
 
 // Routes
 // =============================================================
